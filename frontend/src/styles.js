d90da62f80cb4aebb43046720f665aacbd788e27
Andrei Ungur <andrei.ungur@mail.mcgill.ca>
2019-07-21 20:51:15 -0400

    Display errors more explicitly in UI (#2)

--- frontend/src/styles.js
+++ frontend/src/styles.js
@@ -1,30 +1,51 @@
 const tableStyles = theme => ({
-    root: {
-      width: '100%',
-      marginTop: theme.spacing.unit * 3,
-      overflowX: 'auto',
-    },
-    table: {
-      minWidth: 700,
-    },
-  });
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
   
 const textBoxStyles = theme => ({
-    textField: {
-      marginLeft: theme.spacing.unit,
-      marginRight: theme.spacing.unit,
-    },
-    
-    button: {
-      marginTop: 16,
-    },
-    
-    dense: {
-      marginTop: 16,
-    },
-    menu: {
-      width: 200,
-    },
-  });
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
 
-  export { tableStyles, textBoxStyles }
+  button: {
+    backgroundColor: '#eff0ef',
+    marginTop: 16,
+  },
+    
+  dense: {
+    marginTop: 16,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
+const alertStyles = theme => ({
+  success: {
+    backgroundColor: '#43a047',
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing(1),
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
+
+export { tableStyles, textBoxStyles, alertStyles }
